c088849808dde39577da3103fdd8c0ad503b3c54
Ahmed-Elnemr <ahmedelnemr064@gmail.com>
2023-05-24 12:01:30 +0300

    install vue & create migration

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,11 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Counter;
+use App\Models\Invoice;
+use App\Models\Product;
+use App\Models\Customer;
+use App\Models\InvoiceItem;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -20,5 +25,10 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+        Counter::factory(1)->create();
+        Product::factory(1)->create();
+        Customer::factory(5)->create();
+        Invoice::factory(5)->create();
+        InvoiceItem::factory(5)->create();
     }
 }
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,10 @@
 import './bootstrap';
+import {createApp} from 'vue';
+
+
+
+import app from './components/app.vue';
+
+
+
+createApp(app).mount("#app");
